48c9c6862d028da2bae5755637aa0991d395d017
Daniel Smolak <w63621@student.wsiz.rzeszow.pl>
2020-06-14 18:42:43 +0200

    Init

---
+++ HistoricalQuizGame/HistoricalQuizGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HistoricalQuizGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
